a75772b3e2ff37df72cbf9c2e5d379e403de8448
Alexandre Mommers <alexandre.a.mommers@bnpparibas-pf.com>
2018-03-02 16:14:57 +0100

    init

---
+++ src/main/java/com/bnpp/pf/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.bnpp.pf.config.audit;
---
+++ src/main/java/com/bnpp/pf/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.bnpp.pf.repository;
+
+import com.bnpp.pf.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
